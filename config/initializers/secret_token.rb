b8d71b4f5540e76439ad2df83eb34a15374c8d4c
moredream <moredream7@gmail.com>
2012-08-15 15:13:42 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cropper::Application.config.secret_token = 'a42aa1b77396a2d0ff46e10f6d7128cb2a799d48e7baf2294025c5b09ee126dd078aeaac8f2645a54b70a6cbfabdfba907656d0f35818c0c1979a7bf106cdfbb'
